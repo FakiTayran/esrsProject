4343b89d3c1e23a1db20e2023f71de343e8e1619
fakitayran <aridurufakitayran@gmail.com>
2024-02-16 00:21:27 +0000

    ef migrations done. database and models created

--- realEstateManagementBusinessLayer/Concrete/Spesification/EstateListFilterSpesification.cs
+++ realEstateManagementBusinessLayer/Concrete/Spesification/EstateListFilterSpesification.cs
@@ -3,7 +3,6 @@ using System.ComponentModel.DataAnnotations;
 using System.Xml.Linq;
 using Ardalis.Specification;
 using Microsoft.Extensions.Logging;
-using realEstateManagement.Data;
 using realEstateManagementEntities.Models;
 
 namespace realEstateManagementBusinessLayer.Concrete.Spesification
---
+++ realEstateManagementEntities/Models/AdminUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace realEstateManagementEntities.Models
+{
+    public class AdminUser : IdentityUser
+    {
+        public required string Name { get; set; }
+
+        public required string Surname { get; set; }
+
+        public string? Token { get; set; }
+
+    }
+}
+
