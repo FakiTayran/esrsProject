30e7ff34a1640624d82ab8d283d59b276680aae5
fakitayran <aridurufakitayran@gmail.com>
2024-03-22 16:46:05 +0000

    review mail sender

--- realEstateManagementEntities/Models/Dtos/MailModelDto.cs
+++ realEstateManagementEntities/Models/Dtos/MailModelDto.cs
@@ -6,7 +6,7 @@ namespace realEstateManagementEntities.Models.Dtos
 	public class MailModelDto
 	{
         public required string Subject { get; set; }
-        public required string To { get; set; }
+        public string? To { get; set; }
         public required string Body { get; set; }
         public MailPriority MailPriority { get; set; } = MailPriority.Normal;
     }
